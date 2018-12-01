7cc0bdaea7525fcb21025dfaeef2e8a09d0ff426
Daniel Epstein <d.epstein92@gmail.com>
2018-12-01 00:50:46 -0500

    added NotFound component to App Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../containers/Header';
 import LoginContainer from '../containers/LoginContainer';
 import SignUpContainer from '../containers/SignUpContainer';
+import NotFound from './NotFound';
 import Home from './Home';
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = { hello: '' };
   }
 
@@ -15,10 +16,13 @@ export default class App extends React.Component {
     return (
       <Router>
         <div>
-          <Header />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Login" component={LoginContainer} />
-          <Route exact path="/Sign_Up" component={SignUpContainer} />
+          <Switch>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Login" component={LoginContainer} />
+            <Route exact path="/Sign_Up" component={SignUpContainer} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </Router>
     );
