0a9a1c4a757d4373b672a6b8a2b93068ac08a8ce
allinthemind66 <r.nilon92@gmail.com>
2019-01-11 19:16:49 -0500

    set up state and form event handlers

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -7,19 +7,36 @@ class SignUpContainer extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { hi: '' }
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      city: ""
+    }
+  }
+
+  handleFormInput = (e) => {
+    debugger
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => console.log(this.state))
+  }
+
+  handleFormSubmit = () => {
+    console.log("you are trying to submit")
   }
 
   render() {
     return (
       <div>
         <form action="index.html" method="post">
-            <input type="txt" name="fname" placeholder="first name" required></input>
-            <input type="txt" name="lname" placeholder="last name" required></input>
-            <input type="email" name="email" placeholder="email" required></input>
-            <input type="password" name="pwd" placeholder="password" required></input>
-            <input type="txt" name="location" placeholder="location"></input>
-            <input type="submit"></input> 
+            <input type="txt" name="first_name" placeholder="first name" onChange={this.handleFormInput} value={this.state.first_name} required></input>
+            <input type="txt" name="last_name" placeholder="last name" onChange={this.handleFormInput} value={this.state.last_name} required></input>
+            <input type="email" name="email" placeholder="email" onChange={this.handleFormInput} value={this.state.email} required></input>
+            <input type="password" name="password" placeholder="password" onChange={this.handleFormInput} value={this.state.password} required></input>
+            <input type="txt" name="city" placeholder="city" onChange={this.handleFormInput} value={this.state.city}></input>
+            <input type="submit" onClick={this.handleFormSubmit}></input>
         </form>
       </div>
     );
